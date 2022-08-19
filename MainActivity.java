ade70406341c1e39a931307fff56c7d30c1362f1
SohaHatem22 <103392684+SohaHatem22@users.noreply.github.com>
2022-08-20 01:09:07 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.gridview;
 import java.util.ArrayList;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.view.Menu;
+import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.GridView;
 public class MainActivity extends Activity {
     GridView gv;
     Context context;
     ArrayList prgmName;
+
     public static String [] prgmNameList={"Adidas Bag","Women handbag",
             "Oversize purple shirt", "Oversize grey T-shirt","Black heels",
             "Gold heels","Baggy pants", "Ripped jeans","Les Deux","Nasa Hoodie",
@@ -19,15 +24,24 @@ public class MainActivity extends Activity {
             R.drawable.tshirt1,R.drawable.shoes,R.drawable.heels4,R.drawable.jeanse,
             R.drawable.jeanse1,R.drawable.hoodie2,R.drawable.hoodi5,R.drawable.adidas2,
             R.drawable.adidas};
-    public static String [] prgmNameList2={"800 EGP","900 EGP","300 EGP","350 EGP",
-            "750 EGP","1500 EGP","500 EGP","300 EGP","600 EGP","600 EGP","900 EGP",
-            "1000 EGP"};
+    public static String [] prgmNameList2={"800","900","300","350",
+            "750","1500","500","300","600","600","900",
+            "1000"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button gridActivityBtn =(Button) findViewById(R.id.button3);
+        gridActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Groceries.class);
+                startActivity(i);
+            }
+        });
         gv=(GridView) findViewById(R.id.gridView1);
         gv.setAdapter(new CustomAdapter(this, prgmNameList, prgmImages,prgmNameList2) {
+
         });
     }
 
